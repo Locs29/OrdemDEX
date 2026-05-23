63047953858d48c1bec0990aeb83192dab483696
Eduardo-Luiz-Campos <campos.santos.eduardo@escola.pr.gov.br>
2026-05-23 14:48:46 -0300

    Front funcionando

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+const cors = require('cors');
 //Exportas as rotas
 
 const ameacaRoutes = require('./routes/ameacas'); //rota das ameaças
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 

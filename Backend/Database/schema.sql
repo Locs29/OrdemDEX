316264e0c9b0a6ca98391ae1b0355f8973e01071
Locs29 <eduardolcs14@gmail.com>
2026-08-24 19:53:36 -0300

    Inserido dados no seed.sql

--- Backend/Database/schema.sql
+++ Backend/Database/schema.sql
@@ -1,9 +1,8 @@
-CREATE DATABASE IF NOT EXISTS ordemDEX;
-USE ordemDEX;
+CREATE DATABASE IF NOT EXISTS ordemdex;
+USE ordemdex;
 
 CREATE TABLE IF NOT EXISTS ameacas (
-
-    -- Dados basicos da ameaca
+    -- Dados básicos da ameaça
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL UNIQUE,
     descricao TEXT,
@@ -12,8 +11,9 @@ CREATE TABLE IF NOT EXISTS ameacas (
     vd INT DEFAULT 0,
     tamanho VARCHAR(50),
     defesa INT DEFAULT 0,
-    deslocamento INT DEFAULT 0,
-    
+    deslocamento VARCHAR(50) DEFAULT '9m',
+    presenca_perturbadora VARCHAR(100),
+
     -- Pontos de Vida e Atributos em colunas INT
     pv_max INT DEFAULT 0,
     pv_machucado INT DEFAULT 0,
@@ -22,12 +22,12 @@ CREATE TABLE IF NOT EXISTS ameacas (
     inteligencia INT DEFAULT 0,
     presenca INT DEFAULT 0,
     vigor INT DEFAULT 0,
-    
-    -- Listas em JSON
-    tipos JSON,            -- elementos ameaca
-    pericias JSON,         -- Pericias treinadas da ameaca
-    resistencias JSON,     -- Resistencias da ameaca
-    vulnerabilidades JSON, -- Fraquezas da ameaca
-    acoes JSON,            -- Array de objetos com nome, descrição e dados da ação
-    passivas JSON          -- Array de habilidades passivas
+
+    -- Listas e Objetos Dinâmicos em JSON
+    tipos JSON,            -- Elementos 
+    pericias JSON,         -- Perícias treinadas
+    resistencias JSON,     -- Resistências a dano
+    vulnerabilidades JSON, -- Fraquezas
+    acoes JSON,            -- Ações da criatura
+    passivas JSON,         -- Habilidades passivas
 );

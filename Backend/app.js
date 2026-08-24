bb21813b206eec0f2c6b8fe7b0262305988530a8
Eduardo-Luiz-Campos <campos.santos.eduardo@escola.pr.gov.br>
2026-08-24 17:09:19 -0300

    Arranjando estrutura para expansão

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const cors = require('cors');
 //Exportas as rotas
 
-const ameacaRoutes = require('./routes/ameacas'); //rota das ameaças
+const ameacaRoutes = require('./src/routes/ameacas'); //rota das ameaças
 
 app.use(cors());
 app.use(express.json());
